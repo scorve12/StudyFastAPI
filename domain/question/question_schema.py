d028ed7901c6f96c46fa014fdf98dc7c28b4ee09
scorve12 <scorve5322@gmail.com>
2024-07-04 17:34:25 +0900

    fix validator

--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 
 from domain.answer.answer_schema import Answer
 
@@ -20,7 +20,7 @@ class QuestionCreate(BaseModel):
     subject: str
     content: str
 
-    @validator('subject', 'content')
+    @field_validator('subject', 'content')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
