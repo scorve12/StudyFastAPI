a7c37c5e394e1d5a589ba2cf0170cbb3d92e7f38
scorve12 <scorve5322@gmail.com>
2024-07-08 15:29:15 +0900

    fix uvicorn

--- domain/answer/answer_schema.py
+++ domain/answer/answer_schema.py
@@ -1,11 +1,11 @@
 import datetime
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, validator
 
 class AnswerCreate(BaseModel):
     content: str
 
-    @field_validator('content')
+    @validator('content')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -1,7 +1,7 @@
 import datetime
 import typing
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, validator
 
 from domain.answer.answer_schema import Answer
 
@@ -21,7 +21,7 @@ class QuestionCreate(BaseModel):
     subject: str
     content: str
 
-    @field_validator('subject', 'content')
+    @validator('subject', 'content')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
